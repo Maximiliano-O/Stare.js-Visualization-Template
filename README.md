2efec69c80be4bd36cf74e1de5635a2df3e0cf2c
Maximiliano Ormeño <102495140+Maximiliano-O@users.noreply.github.com>
2025-05-08 18:53:28 -0400

    feat: base of template and Readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Stare.js-Visualization-Template
 StArE.js Template to upload visualizations to the StArE.js Showcase, this is both a template and demo to run a visualization.
+
+This Template is based on stare.js-client made by d-salazar-se
+[text](https://github.com/StArE-js/stare.js-client)
+
+To be more precise it takes the contents of the examples folder, 
+adds the lib folder, which contains visualization files and modifies the
+way main.js works, so instead of the way stare.js-client normally works, as
+in, making a request to stare.js-server, in this case it just loads the info
+of the search from response.json.
+
+The idea is for this to be as self-contained as possible, so is easy to run
+locally, play around with a visualization during dev and then simplify the
+process of sharing it on the showcase site.
+
+## Installation
+
+```bash
+npm install
+```
+## How to use
+
+```bash
+npm run start
+```
+
+Now you can point your browser to
+
+```
+http://localhost:3000/
+```
---
+++ public/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+import axios from 'axios';
+import stare from '../lib/index.js';
+import data from './response.json' assert {type: 'json'};
+
+const STARE_API_URL = '';
+
+(function() {
+  const library = document.querySelector('#library');
+  const query = document.querySelector('#query');
+  const visualization = document.querySelector('#visualization');
+  const visualizeBtn = document.querySelector('#visualizeBtn');
+  const canvas = document.querySelector('#canvas');
+
+  const getResults = (engine, query, pageNumber) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`${STARE_API_URL}/${engine}?query=${query}&pageNumber=${pageNumber}`)
+        .then(response => resolve(response?.data))
+        .catch(error => reject(error));
+    })
+  };
+
+
+  const visualize = () => {
+    console.log('data', data);
+
+    if (!data) {
+      alert('No data to visualize, you must do a query first');
+      return;
+    }
+
+    const chart = stare(library.value, visualization.value);
+
+    if (chart) {
+      document.querySelector('#canvas').innerHTML = '';
+      
+      if (visualization.value === 'grid') {
+        chart('#canvas', data, {});
+      } else if (visualization.value === 'tiles' || visualization.value === 'tiles3') {
+        data.documents.forEach((v, i) =>
+        {
+          let div = document.createElement('div');
+          div.setAttribute('class', 'svg-tiles');
+          let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+          svg.setAttribute('id', 'svg-' + i);
+          div.appendChild(svg);
+          canvas.appendChild(div);
+          chart('#svg-' + i, v, {});
+        });
+      } else {
+        let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('id', 'svg');
+        canvas.appendChild(svg);
+        chart('#svg', data, {});
+      }
+    }
+  }
+
+  visualizeBtn.onclick = () => visualize();
+})();
