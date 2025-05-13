48b7008ca0fe3029c75104069184f61ed3a77867
Maximiliano Ormeño <102495140+Maximiliano-O@users.noreply.github.com>
2025-05-13 12:18:27 -0400

    fix: works locally and on sandbox, data cleanup

--- public/main.js
+++ public/main.js
@@ -1,61 +1,58 @@
-'use strict';
+"use strict";
 
-import axios from 'axios';
-import stare from '../lib/index.js';
-import data from './response.json' assert {type: 'json'};
+import stare from "../lib/index.js";
+import data1 from './data1.json';
 
-const STARE_API_URL = '';
-
-(function() {
-  const library = document.querySelector('#library');
-  const query = document.querySelector('#query');
-  const visualization = document.querySelector('#visualization');
-  const visualizeBtn = document.querySelector('#visualizeBtn');
-  const canvas = document.querySelector('#canvas');
-
-  const getResults = (engine, query, pageNumber) => {
-    return new Promise((resolve, reject) => {
-      axios.get(`${STARE_API_URL}/${engine}?query=${query}&pageNumber=${pageNumber}`)
-        .then(response => resolve(response?.data))
-        .catch(error => reject(error));
-    })
-  };
-
+(function () {
+  const library = document.querySelector("#library");
+  const query = document.querySelector("#query");
+  const visualization = document.querySelector("#visualization");
+  const visualizeBtn = document.querySelector("#visualizeBtn");
+  const canvas = document.querySelector("#canvas");
 
   const visualize = () => {
-    console.log('data', data);
+    const data = data1;
+    console.log("Fetched Data:", data);
 
     if (!data) {
-      alert('No data to visualize, you must do a query first');
+      alert("No data to visualize, you must do a query first");
       return;
     }
 
     const chart = stare(library.value, visualization.value);
 
     if (chart) {
-      document.querySelector('#canvas').innerHTML = '';
-      
-      if (visualization.value === 'grid') {
-        chart('#canvas', data, {});
-      } else if (visualization.value === 'tiles' || visualization.value === 'tiles3') {
-        data.documents.forEach((v, i) =>
-        {
-          let div = document.createElement('div');
-          div.setAttribute('class', 'svg-tiles');
-          let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-          svg.setAttribute('id', 'svg-' + i);
+      document.querySelector("#canvas").innerHTML = "";
+
+      if (visualization.value === "grid") {
+        chart("#canvas", data, {});
+      } else if (
+        visualization.value === "tiles" ||
+        visualization.value === "tiles3"
+      ) {
+        data.documents.forEach((v, i) => {
+          let div = document.createElement("div");
+          div.setAttribute("class", "svg-tiles");
+          let svg = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "svg"
+          );
+          svg.setAttribute("id", "svg-" + i);
           div.appendChild(svg);
           canvas.appendChild(div);
-          chart('#svg-' + i, v, {});
+          chart("#svg-" + i, v, {});
         });
       } else {
-        let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.setAttribute('id', 'svg');
+        let svg = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "svg"
+        );
+        svg.setAttribute("id", "svg");
         canvas.appendChild(svg);
-        chart('#svg', data, {});
+        chart("#svg", data, {});
       }
     }
-  }
+  };
 
   visualizeBtn.onclick = () => visualize();
 })();
