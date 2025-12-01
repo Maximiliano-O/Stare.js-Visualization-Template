3880215045ee726470a3b814e2b7d5843b07db8c
Maximiliano Ormeño <maximiliano.ormeno@usach.cl>
2025-11-30 23:16:23 -0300

    feat: Test-Drive with selectors and two languages

--- src/main.js
+++ src/main.js
@@ -1,43 +1,55 @@
 "use strict";
 
-import chart from "./visualization/bubble.js";
+import bubble from "./visualizations/bubble.js";
+import network from "./visualizations/network.js";
+import treemap from "./visualizations/treemap.js";
+import verticalNetwork from "./visualizations/verticalnetwork.js";
 import data10 from './response/data10.json';
 import data50 from './response/data50.json';
 import data100 from './response/data100.json';
 
+
 (function () {
+  const chartSelector = document.querySelector("#chart");
+  const chartsMap = { bubble, network, treemap, verticalNetwork };
   const dataset = document.querySelector("#dataset");
   const visualizeBtn = document.querySelector("#visualizeBtn");
   const canvas = document.querySelector("#canvas");
 
   const visualize = () => {
-    console.log("Dataset: ", dataset.value)
     let data;
-    if(dataset.value == 'data10'){
-      data = data10;
-    } else if(dataset.value == 'data50'){
-      data = data50;
-    } else {
-      data = data100;
-    }
-    console.log("Fetched Data:", data);
+    if (dataset.value === "data10") data = data10;
+    else if (dataset.value === "data50") data = data50;
+    else data = data100;
 
     if (!data) {
       alert("No data to visualize, you must do a query first");
       return;
     }
 
-    if (chart) {
-      document.querySelector("#canvas").innerHTML = "";
-      let svg = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "svg"
-      );
-      svg.setAttribute("id", "svg");
-      canvas.appendChild(svg);
-      chart("#svg", data, {});
+    const chartFunc = chartsMap[chartSelector.value];
+    if (!chartFunc) {
+      alert("No chart selected or chart function not found");
+      return;
     }
+    
+
+    canvas.innerHTML = "";
+    if (chartSelector.value === "verticalNetwork") {
+      chartFunc("#canvas", data, {});
+    } else {
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      svg.setAttribute("id", "svg");
+      svg.setAttribute("width", canvas.clientWidth);
+      svg.setAttribute("height", canvas.clientHeight);
+      canvas.appendChild(svg);
+
+      chartFunc("#svg", data, {});
+}
   };
 
-  visualizeBtn.onclick = () => visualize();
+  visualizeBtn.addEventListener("click", visualize);
+
+  window.onload = visualize;
 })();
+
